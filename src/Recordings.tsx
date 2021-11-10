c749af1c17e427370ed64d18a45eea724de7d056
Thomas Ritter <thomas.ritter@mocoapp.com>
2021-11-11 00:33:01 +0100

    styling, improve UX

--- src/Recordings.tsx
+++ src/Recordings.tsx
@@ -2,28 +2,45 @@ import React, { useRef, useState } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 
 const Recordings: React.FC = () => {
-  const [blobUrls, setBlobUrls] = useState<[string, Blob][]>([]);
+  const [blobUrls, setBlobUrls] = useState<
+    { url: string; blob: Blob; label: string }[]
+  >([]);
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
   // const [currentDevice, setCurrentDevice] = useState<string | null>(null);
-  const audioConstraint = useRef<{ deviceId: { exact: string } } | true>(true);
+  const audioConstraint = useRef<{ deviceId: { exact: string } | undefined }>({
+    deviceId: undefined,
+  });
+  const audioSource = useRef<HTMLSelectElement>(null);
 
   const onStop = (blobUrl: string, blob: Blob) => {
     setBlobUrls((prev) => {
       if (blobUrl) {
-        return [...prev, [blobUrl, blob]];
+        return [
+          ...prev,
+          {
+            url: blobUrl,
+            blob,
+            label:
+              audioSource.current?.options[
+                audioSource.current?.options.selectedIndex
+              ].label || "<unknown>",
+          },
+        ];
       } else {
         return prev;
       }
     });
+    listDevices();
   };
 
   const listDevices = async () => {
-    // await window.navigator.mediaDevices.getUserMedia({
-    //   audio: true,
-    //   video: false,
-    // });
+    const stream = await window.navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
     const devices = await window.navigator.mediaDevices.enumerateDevices();
     setDevices(devices.filter((device) => device.kind === "audioinput"));
+    stream.getTracks().forEach((track) => track.stop());
   };
 
   const { status, startRecording, stopRecording, previewAudioStream, error } =
@@ -31,18 +48,17 @@ const Recordings: React.FC = () => {
       video: false,
       audio: audioConstraint.current,
       onStop,
-      askPermissionOnMount: true,
     });
 
   const onNewRecording = () => {
     startRecording();
-    // listDevices();
+    listDevices();
   };
 
   console.log("render", audioConstraint.current);
 
   return (
-    <div>
+    <main>
       <h1>Recordings!</h1>
       <div>Status: {status}</div>
       <div>Error: {error}</div>
@@ -50,8 +66,10 @@ const Recordings: React.FC = () => {
         Devices:
         <select
           id="audioSource"
+          ref={audioSource}
           onChange={(ev) => {
             audioConstraint.current = { deviceId: { exact: ev.target.value } };
+            listDevices();
           }}
         >
           {devices.map((device, index) => (
@@ -65,21 +83,26 @@ const Recordings: React.FC = () => {
         </button>
       </div>
       <div>
-        <button type="button" onClick={onNewRecording}>
-          New Recording
-        </button>
-        <button type="button" onClick={stopRecording}>
-          Stop Recording
-        </button>
+        {status !== "recording" && (
+          <button type="button" onClick={onNewRecording}>
+            New Recording
+          </button>
+        )}
+        {status === "recording" && (
+          <button type="button" onClick={stopRecording}>
+            Stop Recording
+          </button>
+        )}
       </div>
       <div>
-        {blobUrls.map(([url, blob], index) => (
+        {blobUrls.map((blob, index) => (
           <div key={index}>
-            <audio src={url} controls />
+            {blob.label}
+            <audio src={blob.url} controls />
           </div>
         ))}
       </div>
-    </div>
+    </main>
   );
 };
 
