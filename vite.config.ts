5baa65b5322d4bcfa047f7431b230878c518ac1e
Thomas Ritter <thomas.ritter@mocoapp.com>
2023-04-24 20:07:46 +0200

    upgrade vite to 2.x latest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: true,
+    // https: true,
   },
   base: "",
   plugins: [mkcert(), react()],
