1183929fb66c048c2178284d4bac649698941c01
Thomas Ritter <thomas.ritter@mocoapp.com>
2023-04-25 09:09:10 +0200

    remove mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // https: true,
-  },
+  server: {},
   base: "",
-  plugins: [mkcert(), react()],
+  plugins: [react()],
 });
